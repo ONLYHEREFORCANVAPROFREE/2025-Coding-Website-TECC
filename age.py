b82d2057b5035006bf36b5f245b09b11258b3e02
ONLYHEREFORCANVAPROFREE <stu23506@ashs.school.nz>
2025-06-21 22:49:59 +1200

    Testing GitHub and submitting initial commit.

---
+++ age.py
@@ -0,0 +1,24 @@
+age = (input("please enter your age: "))
+if age<18:
+    print ("sorry you are too young")
+elif age>60:
+    print ("sorry you are too old")
+else:
+    print ("Congrats! You are eligible to work")
+    
+    
+    
+age = int(input("please enter your age: "))
+if age>=18 and age <=60:
+    print ("congrats!")
+elif:
+print ("sorry you are too old")
+else:
+    print ("sorry")
+    
+    
+age = int(input("Please enter your age: "))
+if age=>18 and age<=60:
+    print("Congrats! Your age is just right!")
+else age >60:
+    print("Sorry, you're too old!")
